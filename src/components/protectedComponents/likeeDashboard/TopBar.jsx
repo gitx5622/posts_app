5de92c44ac643c992c6436f5a0ee5f52fc6d6276
gitx5622 <ggitau5622@gmail.com>
2021-06-01 12:49:05 +0300

    Topbar

--- src/components/protectedComponents/likeeDashboard/TopBar.jsx
+++ src/components/protectedComponents/likeeDashboard/TopBar.jsx
@@ -1,16 +1,80 @@
 import React, { Fragment} from 'react';
+import {withRouter} from "react-router-dom";
 
 
-const TopBar = () => {
+const TopBar = ({
+    match: {
+        params: { dashboard_route },
+    },
+    history,
+    }) => {
+    const UnauthenticatedLinks = withRouter(() => {
+        return (
+            <div className="middle-section">
+                <div
+                    className={`link ${
+                        dashboard_route === undefined || dashboard_route === 'posts'
+                            ? 'active'
+                            : ''
+                    }`}
+                    onClick={() => {
+                        history.push(`/posts`);
+                        document.querySelector('.main-content').scroll({
+                            top: 0,
+                        });
+                    }}
+                >
+                    <span>Posts</span>
+                </div>
+                <div
+                    className={`link ${
+                        dashboard_route === undefined || dashboard_route === 'posts'
+                            ? 'active'
+                            : ''
+                    }`}
+                    onClick={() => {
+                        history.push(`/posts`);
+                        document.querySelector('.main-content').scroll({
+                            top: 0,
+                        });
+                    }}
+                >
+                    <span>Home</span>
+                </div>
+                <div
+                    className={`link ${
+                        dashboard_route === undefined || dashboard_route === 'posts'
+                            ? 'active'
+                            : ''
+                    }`}
+                    onClick={() => {
+                        history.push(`/posts`);
+                        document.querySelector('.main-content').scroll({
+                            top: 0,
+                        });
+                    }}
+                >
+                    <span>Home</span>
+                </div>
+            </div>
+        )
+    })
+    const AuthenticatedLinks = withRouter(() => {
+        return (
+            <div className="middle-section">
+                <div>Home</div>
+            </div>
+        )
+    })
     return (
         <Fragment>
             <div className="topbar">
-                <div><h3>Start</h3></div>
-                <div><h3>Middle</h3></div>
-                <div><h3>End</h3></div>
+                <div><h2>Post App</h2></div>
+                <div><UnauthenticatedLinks/></div>
+                <div>ddd</div>
             </div>
         </Fragment>
     )
 };
 
-export default TopBar;
+export default withRouter(TopBar);
